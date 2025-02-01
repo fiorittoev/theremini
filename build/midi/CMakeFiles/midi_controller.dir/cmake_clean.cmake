9f17962feee85d262bc699ca2ad798adaecb0206
fiorittoev <fiorittoev@gmail.com>
2025-02-01 16:13:41 -0500

    AHHHHHH

---
+++ build/midi/CMakeFiles/midi_controller.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/midi_controller.dir/main.cpp.obj"
+  "CMakeFiles/midi_controller.dir/main.cpp.obj.d"
+  "midi_controller.pdb"
+  "midi_controller.wasm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/midi_controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
